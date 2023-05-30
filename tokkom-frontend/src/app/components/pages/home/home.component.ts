98f50290bb648daec3a4d50f434f156acbf56515
Made Manik Datu Yasa <manikyasa@gmail.com>
2023-05-30 13:58:49 +0700

    feat: add dashboard summary product, order, notif

--- tokkom-frontend/src/app/components/pages/home/home.component.ts
+++ tokkom-frontend/src/app/components/pages/home/home.component.ts
@@ -3,7 +3,10 @@ import { MatPaginator } from '@angular/material/paginator';
 import { AddProductComponent } from '../../panel/products/add-product/add-product.component';
 import { MatTableDataSource } from '@angular/material/table';
 import { ProductService } from 'src/app/services/product/product.service';
+import { OrderService } from 'src/app/services/order/order.service';
+import { NotificationService } from 'src/app/services/notification/notification.service';
 import { Product } from 'src/app/models/product.model';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -12,6 +15,10 @@ import { Product } from 'src/app/models/product.model';
 })
 export class HomeComponent implements AfterViewInit, OnInit {
   displayedColumns: string[] = ['title', 'brand', 'category', 'stock', 'price'];
+
+  productSummary: number = 0;
+  orderSummary: number = 0;
+  notifSummary: number = 0;
 
   dataSource = new MatTableDataSource<Product>();
   ELEMENT_DATA = [];
@@ -22,20 +29,54 @@ export class HomeComponent implements AfterViewInit, OnInit {
     this.dataSource.paginator = this.paginator;
   }
 
-  constructor(private productService: ProductService) {}
+  constructor(
+    private productService: ProductService,
+    private orderService: OrderService,
+    private notificationService: NotificationService
+  ) {}
 
   ngOnInit(): void {
+    this.getOrders();
     this.getProducts();
+    this.getNotifs();
   }
 
   getProducts() {
     this.productService.getDataProducts().subscribe({
       next: (res) => {
         this.dataSource.data = res;
+        this.productSummary = res.length;
       },
       error: (err) => {
-        alert(err);
+        console.log(err);
       },
     });
+  }
+
+  getOrders() {
+    this.orderService.getAllOrders().subscribe({
+      next: (res) => {
+        this.orderSummary = res.length;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
+  getNotifs() {
+    this.notificationService
+      .getNotifications()
+      .pipe(
+        map((notif: any[]) => notif.filter((not: any) => not.isExpired === 0))
+      )
+      .subscribe({
+        next: (res) => {
+          this.notifSummary = res.length;
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
   }
 }
